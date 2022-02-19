1cd54467f561d13964c8f29d38f94929f9144af9
TuanNA <tuanna1906@gmail.com>
2022-02-19 16:07:46 +0900

    Marketplace | Home page

---
+++ marketplace/components/Hero.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Image from "next/image";
+
+import meotwo from "../assets/150.png";
+import meo from "../assets/151.png";
+
+const style = {
+  wrapper: `relative`,
+  container: `before:content-[''] before:bg-red-300 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[url('../assets/150.png')] before:bg-cover before:bg-center before:opacity-30 before:blur`,
+  contentWrapper: `flex h-screen relative justify-center flex-wrap items-center`,
+  copyContainer: `w-1/2`,
+  title: `relative text-white text-[46px] font-semibold`,
+  description: `text-[#8a939b] container-[400px] text-2xl mt-[0.8rem] mb-[2.5rem]`,
+  ctaContainer: `flex`,
+  accentedButton: ` relative text-lg font-semibold px-12 py-4 bg-[#2181e2] rounded-lg mr-5 text-white hover:bg-[#42a0ff] cursor-pointer`,
+  button: ` relative text-lg font-semibold px-12 py-4 bg-[#363840] rounded-lg mr-5 text-[#e4e8ea] hover:bg-[#4c505c] cursor-pointer`,
+  cardContainer: `rounded-[3rem]`,
+  infoContainer: `h-20 bg-[#313338] p-4  rounded-lg flex items-center text-white`,
+  author: `flex flex-col justify-center ml-4`,
+  name: ``,
+  infoIcon: `flex justify-end items-center flex-1 text-[#8a939b] text-3xl font-bold`,
+};
+
+const Hero = () => {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <div className={style.contentWrapper}>
+          <div className={style.copyContainer}>
+            <div className={style.title}>
+              Discover, collect, and sell extraordinary NFTs
+            </div>
+            <div className={style.description}>
+              The world&apos;s first and largest NFT marketplace
+            </div>
+            <div className={style.ctaContainer}>
+              <button className={style.accentedButton}>Explore</button>
+              <button className={style.button}>Create</button>
+            </div>
+          </div>
+          <div className={style.cardContainer}>
+            <Image src={meotwo} className="rounded-t-lg"></Image>
+            <div className={style.infoContainer}>
+              <Image
+                src={meo}
+                className="h-[2.25rem] rounded-full"
+                height={40}
+                width={40}
+              ></Image>
+              <div className={style.author}>
+                <div className={style.name}>TuanNA</div>
+                <a className="text-[#1868b7]" href="/kira-ng">
+                  kira-ng
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
