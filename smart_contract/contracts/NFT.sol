4dfe8166b03eff6ec406793e5b7b7f356bdfe89f
TuanNA <tuanna1906@gmail.com>
2022-02-19 12:33:09 +0900

    NFT Smart Contract

---
+++ smart_contract/contracts/NFT.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contracAddress;
+
+    constructor(address marketplaceAddress) ERC721("NewIT NFT", "NINFT") {
+        contracAddress = marketplaceAddress;
+    }
+
+    function mintNFT(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contracAddress, true);
+
+        return newItemId;
+    }
+}
